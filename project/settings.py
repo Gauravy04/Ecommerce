f8bdeff0bac1fa44e4fc20c0a278e37c0079f477
gourav <gourav@gamil.com>
2025-03-31 19:24:27 +0530

    two

--- project/settings.py
+++ project/settings.py
@@ -133,3 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+ALLOWED_HOSTS = ['ecommerce-0g8d.onrender.com', '127.0.0.1', 'localhost']
